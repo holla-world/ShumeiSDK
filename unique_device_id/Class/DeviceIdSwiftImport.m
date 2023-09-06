7c543f493b23ade495f5817b526411276ec4e404
maopenglin <maopenglin@gmail.com>
2023-09-06 11:12:08 +0800

    如果有id，就不初始化了

--- unique_device_id/Class/DeviceIdSwiftImport.m
+++ unique_device_id/Class/DeviceIdSwiftImport.m
@@ -17,17 +17,17 @@
 
 @implementation DeviceIdSwiftImport
 + (void)updateAppId:(NSString *)appId{
-    SmOption *options = [[SmOption alloc] init];
-    [options setAppId:appId];
-    NSString* host = @"https://shumei.servicecloudweb.com";
-    [options setUrl:[host stringByAppendingString:@"/v3/profile/ios"]];
-    [options setConfUrl:[host stringByAppendingString:@"/v3/cloudconf"]];
-    [options setOrganization:@"IZf9gGUfAMI9GSg5xh0v"];
-    [options setPublicKey:@"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+Tk+uhiG0PHuSP4lnqtl1xcLQCXdKAEPB22HlPVDafWHZNu3A/j4dUJ4K+LwRNLl/irJuFK0FQ8lTO4EHsdIKnUV4zaRx+fBAML5suVvUNu6x71EnIB7+gxF7eKALiAIwYCaf/Siaxc7WIyi3f8BJ0nBAnbbkj1YggfTdY0G7FQIDAQAB"];
-    [[SmAntiFraud shareInstance] create:options];
-    
     NSString * sid = [UICKeyChainStore stringForKey:@"smServerId"];
     if (sid == NULL) {
+        SmOption *options = [[SmOption alloc] init];
+        [options setAppId:appId];
+        NSString* host = @"https://shumei.servicecloudweb.com";
+        [options setUrl:[host stringByAppendingString:@"/v3/profile/ios"]];
+        [options setConfUrl:[host stringByAppendingString:@"/v3/cloudconf"]];
+        [options setOrganization:@"IZf9gGUfAMI9GSg5xh0v"];
+        [options setPublicKey:@"MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC+Tk+uhiG0PHuSP4lnqtl1xcLQCXdKAEPB22HlPVDafWHZNu3A/j4dUJ4K+LwRNLl/irJuFK0FQ8lTO4EHsdIKnUV4zaRx+fBAML5suVvUNu6x71EnIB7+gxF7eKALiAIwYCaf/Siaxc7WIyi3f8BJ0nBAnbbkj1YggfTdY0G7FQIDAQAB"];
+        [[SmAntiFraud shareInstance] create:options];
+        
         [SmAntiFraud.shareInstance  registerServerIdCallback:[DeviceIdSwiftImport current]];
     }
 }
